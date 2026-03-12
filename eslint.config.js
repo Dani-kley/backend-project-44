4ce7c8f65c1c56e81ca4afc92aa44e8c963b76da
Danil <kingstudy.fedoseenko@gmail.com>
2026-03-12 19:15:59 +0100

    fix Eslint 2

--- eslint.config.js
+++ eslint.config.js
@@ -6,4 +6,4 @@ import { defineConfig } from 'eslint/config'
 export default defineConfig([
   { files: ['**/*.{js,mjs,cjs}'], plugins: { js }, extends: ['js/recommended'], languageOptions: { globals: globals.browser } },
   { files: ['**/*.json5'], plugins: { json }, language: 'json/json5', extends: ['json/recommended'] },
-]);
+])
