c29b3dff0de5dd2e8ef2857ef23356879bc95ebd
Danil <kingstudy.fedoseenko@gmail.com>
2026-03-12 18:06:32 +0100

    Add progression

--- bin/brain-progression.js
+++ bin/brain-progression.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-import { runGcdGame } from '../src/gcd.js';
+import { runProgressionGame } from '../src/progression.js';
 console.log('Welcome to the Brain Games!');
 const name = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${name}!`);
-runGcdGame(name);
+runProgressionGame(name);
